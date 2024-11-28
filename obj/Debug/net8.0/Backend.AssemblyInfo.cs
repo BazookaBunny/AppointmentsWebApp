e598f3b89f2d8bac0abacb353bff14f0eaa7f4cb
BazookaBunny <deadblesreaper@yahoo.com>
2024-11-28 15:16:36 +0200

    first commit

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eea4fb2bbfdb0915d1ffa38cb0b46b9034223a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcaa149e7eeb78e87a7e52c5c52264444a49851c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
